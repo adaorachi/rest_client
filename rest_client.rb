4a9c9f2fa898297251bcb72561b98e4f55edd073
FelipeEnne <felipeenne@gmail.com>
2019-12-04 18:03:56 -0300

    Adding class for the request method

--- rest_client.rb
+++ rest_client.rb
@@ -1,5 +1,6 @@
 require 'rest-client'
 
+=begin
 puts "What do you want to search for? "
 input = gets.chomp
 
@@ -10,12 +11,27 @@ response = RestClient.get "https://www.bing.com/search?q=#{input}"
 
 print "HTTP response code is #{response.code}\n"
 print "HTTP response body is #{response.body}\n"
+=end
 
 class Bing
+  def call_search(inp)
+    inp = inp.split(" ")
+    inp = inp.join("+")
+  end
 
-  def get_chomp(inp)
-    input = gets.chomp
-    # input = input.split(" ")
-    # input = input.join("+")
+  def get_response(inp)
+    response = RestClient.get "https://www.bing.com/search?q=#{inp}"
+  end
+
+  def print_response(response)
+    print "HTTP response code is #{response.code}\n"
+    print "HTTP response body is #{response.body}\n"
   end
 end
+
+a = Bing.new
+
+input = gets.chomp
+input = a.call_search(input)
+res = a.get_response(input)
+a.print_response(res)
