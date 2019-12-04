aac47d91aca568871eb7754483dfe7cba6cc369d
FelipeEnne <felipeenne@gmail.com>
2019-12-04 18:22:16 -0300

    Fix linter errors

--- rest_client.rb
+++ rest_client.rb
@@ -1,26 +1,14 @@
+# frozen_string_literal: true
+
 require 'rest-client'
-
-=begin
-puts "What do you want to search for? "
-input = gets.chomp
-
-input = input.split(" ")
-input = input.join("+")
-
-response = RestClient.get "https://www.bing.com/search?q=#{input}"
-
-print "HTTP response code is #{response.code}\n"
-print "HTTP response body is #{response.body}\n"
-=end
 
 class Bing
   def call_search(inp)
-    inp = inp.split(" ")
-    inp = inp.join("+")
+    inp.split(' ').join('+')
   end
 
   def get_response(inp)
-    response = RestClient.get "https://www.bing.com/search?q=#{inp}"
+    RestClient.get "https://www.bing.com/search?q=#{inp}"
   end
 
   def print_response(response)
